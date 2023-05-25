2773f5c241dbb3e4ef46269e255f8ddd7ae744ed
vjimcor955 <vjimcor955@g.educaand.es>
2023-05-25 16:21:57 +0200

    menu lateral funciona y responsive completo

--- src/app.js
+++ src/app.js
@@ -1,1 +1,6 @@
-console.log('Hello world!');
+var icon = document.getElementById('icono-menu')
+var menu = document.getElementById('menu-lateral')
+
+icon.addEventListener('click', function() {
+    menu.classList.toggle('open')
+})
